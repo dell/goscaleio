f91969ba02dfa963a474a78098f818898a0fa92a
Kshitija Kakde <Kshitija_Kakde@Dell.com>
2023-04-19 09:37:41 +0530

    removed commented code

--- nfs_export.go
+++ nfs_export.go
@@ -1,3 +1,15 @@
+// Copyright © 2019 - 2023 Dell Inc. or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package goscaleio
 
 import (
@@ -8,16 +20,6 @@ import (
 
 	types "github.com/dell/goscaleio/types/v1"
 )
-
-// type NFSExportCreate struct {
-// 	// NFS Export name
-// 	Name string `json:"name"`
-// 	// NFS Export description
-// 	// Unique identifier of the file system on which the NFS Export was created
-// 	FileSystemID string `json:"file_system_id"`
-// 	// Local path to a location within the file system.
-// 	Path string `json:"path"`
-// }
 
 // NGetNFSExport lists NFS Exports.
 func (c *Client) GetNFSExport() (nfsList []types.NFSExport, err error) {
