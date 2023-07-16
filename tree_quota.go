f5d8eea52ef71e9313dc33b6df611467553eecc9
Kshitija Kakde <Kshitija_Kakde@Dell.com>
2023-07-17 00:25:54 +0530

    tree quota related changes

--- tree_quota.go
+++ tree_quota.go
@@ -3,19 +3,71 @@ package goscaleio
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	types "github.com/dell/goscaleio/types/v1"
 )
 
-// CreateNFSExport create an NFS Export for a File System.
-func (c *Client) CreateTreeQuota(createParams *types.TreeQuotaCreate) (respnfs *types.TreeQuotaCreateResponse, err error) {
-	path := fmt.Sprintf("/rest/v1//file-tree-quotas")
+func (s *System) GetTreeQuota() (treeQuotaList []types.TreeQuota, err error) {
+	defer TimeSpent("GetTreeQuota", time.Now())
+	path := fmt.Sprintf("/rest/v1/file-tree-quotas?select=*")
 
-	var body *types.TreeQuotaCreate = createParams
-	err = c.getJSONWithRetry(http.MethodPost, path, body, &respnfs)
+	err = s.client.getJSONWithRetry(
+		http.MethodGet, path, nil, &treeQuotaList)
 	if err != nil {
 		return nil, err
 	}
 
-	return respnfs, nil
+	return treeQuotaList, nil
+}
+
+func (s *System) GetTreeQuotaByID(id string) (treeQuota *types.TreeQuota, err error) {
+	defer TimeSpent("GetTreeQuota", time.Now())
+	path := fmt.Sprintf("/rest/v1/file-tree-quotas/%s?select=*", id)
+
+	err = s.client.getJSONWithRetry(
+		http.MethodGet, path, nil, &treeQuota)
+	if err != nil {
+		return nil, err
+	}
+
+	return treeQuota, nil
+}
+
+// CreateNFSExport create an NFS Export for a File System.
+func (s *System) CreateTreeQuota(createParams *types.TreeQuotaCreate) (resp *types.TreeQuotaCreateResponse, err error) {
+	path := fmt.Sprintf("/rest/v1/file-tree-quotas")
+
+	var body *types.TreeQuotaCreate = createParams
+	err = s.client.getJSONWithRetry(http.MethodPost, path, body, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (s *System) ModifyTreeQuota(ModifyParams *types.TreeQuotaModify, id string) (err error) {
+	path := fmt.Sprintf("/rest/v1/file-tree-quotas/%s", id)
+
+	var body *types.TreeQuotaModify = ModifyParams
+	err = s.client.getJSONWithRetry(http.MethodPatch, path, body, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *System) DeleteTreeQuota(id string) error {
+	defer TimeSpent("DeleteTreeQuota", time.Now())
+	path := fmt.Sprintf("/rest/v1/file-tree-quotas/%s", id)
+
+	err := s.client.getJSONWithRetry(
+		http.MethodDelete, path, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- types/v1/types.go
+++ types/v1/types.go
@@ -1175,10 +1175,6 @@ type CreateNASResponse struct {
 	ID string `json:"id"`
 }
 
-type TreeQuotaCreateResponse struct {
-	ID string `json:"id"`
-}
-
 // CreateNASParam defines the struct for CreateNASParam
 type CreateNASParam struct {
 	Name                         string `json:"name"`
@@ -1252,8 +1248,12 @@ type FsCreate struct {
 
 // FSModify defines struct for modify FS
 type FSModify struct {
-	Size        int    `json:"size_total"`
-	Description string `json:"description,omitempty"`
+	Size             int    `json:"size_total,omitempty"`
+	Description      string `json:"description,omitempty"`
+	IsQuotaEnabled   bool   `json:"is_quota_enabled,omitempty"`
+	GracePeriod      int    `json:"grace_period,omitempty"`
+	DefaultHardLimit int    `json:"default_hard_limit,omitempty"`
+	DefaultSoftLimit int    `json:"default_soft_limit,omitempty"`
 }
 
 // FileSystemResp defines struct for FileSystemResp
@@ -1292,6 +1292,10 @@ type NFSExportCreateResponse struct {
 	ID string `json:"id"`
 }
 
+type TreeQuotaCreateResponse struct {
+	ID string `json:"id"`
+}
+
 // NFSExportCreate defines struct for Create NFS Export
 type NFSExportCreate struct {
 	Name               string   `json:"name"`
@@ -1303,18 +1307,39 @@ type NFSExportCreate struct {
 	ReadOnlyRootHosts  []string `json:"read_only_root_hosts,omitempty"`
 	ReadWriteRootHosts []string `json:"read_write_root_hosts,omitempty"`
 	AnonymousUID       int      `json:"anonymous_UID,omitempty"`
-	AnonymousGID       int      `json:"anonymous_GID,omiempty"`
+	AnonymousGID       int      `json:"anonymous_GID,omitempty"`
 	IsNoSUID           bool     `json:"is_no_SUID,omitempty"`
 }
 
 type TreeQuotaCreate struct {
-	FileSysytemID       string `json:"file_system_id"`
+	FileSystemID        string `json:"file_system_id"`
 	Path                string `json:"path"`
 	Description         string `json:"description,omitempty"`
 	Hard_Limit          int    `json:"hard_limit,omitempty"`
 	Soft_Limit          int    `json:"soft_limit,omitempty"`
 	IsUserQuotaEnforced bool   `json:"is_user_quotas_enforced,omitempty"`
 	GracePeroid         int    `json:"grace_period,omitempty"`
+}
+
+type TreeQuota struct {
+	ID                   string
+	FileSysytemID        string `json:"file_system_id"`
+	Path                 string `json:"path"`
+	Description          string `json:"description,omitempty"`
+	Hard_Limit           int    `json:"hard_limit,omitempty"`
+	Soft_Limit           int    `json:"soft_limit,omitempty"`
+	IsUserQuotaEnforced  bool   `json:"is_user_quotas_enforced,omitempty"`
+	GracePeroid          int    `json:"grace_period,omitempty"`
+	State                string `json:"state,omitempty"`
+	RemainingGracePeroid int    `json:"remaining_grace_period,omitempty"`
+	SizeUsed             int    `json:"size_used,omitempty"`
+}
+type TreeQuotaModify struct {
+	Description          string `json:"description,omitempty"`
+	HardLimit            int    `json:"hard_limit,omitempty"`
+	SoftLimit            int    `json:"soft_limit,omitempty"`
+	IsUserQuotasEnforced bool   `json:"is_user_quotas_enforced,omitempty"`
+	GracePeroid          int    `json:"grace_period,omitempty"`
 }
 
 // NFSExportModify defines struct for Modify NFS Export
