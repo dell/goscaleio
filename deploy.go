956097487188f58d4ecf3b46e9c1781bf4f57bde
root <krunal.thakkar@dell.com>
2023-12-11 07:27:44 -0500

    Auth change for 4.5 Deployment

--- deploy.go
+++ deploy.go
@@ -438,7 +438,9 @@ func (gc *GatewayClient) ValidateMDMDetails(mdmTopologyParam []byte) (*types.Gat
 
 		return &gatewayResponse, nil
 	} else if httpResp.StatusCode == 200 && responseString == "" {
-		return &gatewayResponse, errors.New("Wrong Primary MDM IP, Please provide valid Primary MDM IP")
+		gatewayResponse.Message = "Wrong Primary MDM IP, Please provide valid Primary MDM IP"
+
+		return &gatewayResponse, fmt.Errorf("Wrong Primary MDM IP, Please provide valid Primary MDM IP")
 	}
 
 	if gc.version == "4.0" {
