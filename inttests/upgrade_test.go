6fc2fbf5680e0f0b02927e2ffcba3e9dec92c55a
AnikaAgiwal2711 <118799587+AnikaAgiwal2711@users.noreply.github.com>
2024-05-09 14:50:06 +0530

    added changes for uploading the compliance file (#112)
    * added changes for uploading the compliance file

---
+++ inttests/upgrade_test.go
@@ -0,0 +1,51 @@
+// Copyright © 2024 Dell Inc. or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package inttests
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	types "github.com/dell/goscaleio/types/v1"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestDeployUploadPackage function to test upload packge with dummy path of packages
+func TestUploadCompliance(t *testing.T) {
+	var sourceLocation string
+	if os.Getenv("GOSCALEIO_COMPLIANCE_ENDPOINT") != "" {
+		sourceLocation = os.Getenv("GOSCALEIO_COMPLIANCE_ENDPOINT")
+	}
+	ucParam := &types.UploadComplianceParam{
+		SourceLocation: sourceLocation,
+	}
+	details, err := GC.UploadCompliance(ucParam)
+	assert.Nil(t, err)
+	assert.NotNil(t, details.ID)
+	assert.NotNil(t, details.State)
+	time.Sleep(5 * time.Second)
+	indepthDetails, err := GC.GetUploadComplianceDetails(details.ID)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, indepthDetails.ID)
+	assert.NotEmpty(t, indepthDetails.State)
+}
+
+func TestApproveUnsignedFile(t *testing.T) {
+	var unsigned string
+	if os.Getenv("GOSCALEIO_UNSIGNED_COMPLIANCE_FILE_ID") != "" {
+		unsigned = os.Getenv("GOSCALEIO_UNSIGNED_COMPLIANCE_FILE_ID")
+	}
+	err := GC.ApproveUnsignedFile(unsigned)
+	assert.Nil(t, err)
+}
---
+++ upgrade_test.go
@@ -0,0 +1,63 @@
+// Copyright © 2024 Dell Inc. or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package goscaleio
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	types "github.com/dell/goscaleio/types/v1"
+)
+
+// This test can be checked when NewGateway() function is fixed
+func TestUploadCompliance(t *testing.T) {
+	t.Skip("Skipping this test case")
+	type testCase struct {
+		ucParam  *types.UploadComplianceParam
+		expected error
+	}
+	cases := []testCase{
+		{
+			ucParam: &types.UploadComplianceParam{
+				SourceLocation: "https://10.10.10.1/artifactory/pfmp20/RCM/Denver/RCMs/SoftwareOnly/PowerFlex_Software_4.5.0.0_287_r1.zip",
+			},
+			expected: nil,
+		},
+	}
+	svr := httptest.NewServer(http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
+	}))
+	defer svr.Close()
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run("", func(_ *testing.T) {
+			GC, err := NewGateway(svr.URL, "", "", true, true)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			_, errFs = GC.UploadCompliance(tc.ucParam)
+			if errFs != nil {
+				if tc.expected == nil {
+					t.Errorf("Uploading Compliance File did not work as expected, \n\tgot: %s \n\twant: %v", errFs, tc.expected)
+				} else {
+					if errFs.Error() != tc.expected.Error() {
+						t.Errorf("Uploading Compliance File did not work as expected, \n\tgot: %s \n\twant: %s", errFs, tc.expected)
+					}
+				}
+			}
+		})
+	}
+}
