fec362d24fcd6100bc1fe7197db69f8999d3e08b
JacobGros <jacobgrosner4@gmail.com>
2025-03-05 15:03:45 -0500

    add UT for log

--- log/log.go
+++ log/log.go
@@ -1,3 +1,15 @@
+// Copyright © 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package log
 
 import (
@@ -7,14 +19,17 @@ import (
 
 var (
 	logLevel = new(slog.LevelVar) // Info by default
-	Log      = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: logLevel}))
-	Debug    = false // False by default, will turn true if level is set to debug
+	// Log is a logger for goscaleio and api packages to use
+	Log   = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: logLevel}))
+	debug = false // False by default, will turn true if level is set to debug
 )
 
 func SetLogLevel(level slog.Level) {
 	logLevel.Set(level)
 	if level == slog.LevelDebug {
-		Debug = true
+		debug = true
+	} else {
+		debug = false
 	}
 }
 
@@ -22,7 +37,7 @@ func DoLog(
 	l func(msg string, args ...any),
 	msg string,
 ) {
-	if Debug {
+	if debug {
 		l(msg)
 	}
 }
