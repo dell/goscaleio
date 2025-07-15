4a5e3f3c67137e7c0b655ba6712762cf2856d2c1
Bahubali Jain <bahubali.jain@dell.com>
2025-07-15 00:42:01 -0500

    formatting changes

--- sdc.go
+++ sdc.go
@@ -201,7 +201,6 @@ func (sdc *Sdc) GetVolumeMetrics() ([]*types.SdcVolumeMetrics, error) {
 	path := fmt.Sprintf("/api/instances/Sdc::%s/action/queryVolumeSdcBwc", sdcID)
 
 	var body = struct{}{}
-
 	var metrics []*types.SdcVolumeMetrics
 	err := sdc.client.getJSONWithRetry(http.MethodPost, path, body, &metrics)
 	if err != nil {
