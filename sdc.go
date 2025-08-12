bc973a52866fa07b4765d32a39948e9a7b49f01b
Rishabh Raj <rishabh.raj2@dell.com>
2025-08-12 10:22:27 -0500

    added error handling in approveSDC

--- sdc.go
+++ sdc.go
@@ -381,6 +381,8 @@ func (s *System) ApproveSdc(approveSdcParam *types.ApproveSdcParam) (*types.Appr
 		// Allow only the SDC with this GUID
 		if approveSdcParam.SdcGUID != "" {
 			sdcParam.SdcGUID = approveSdcParam.SdcGUID
+		} else {
+			return nil, fmt.Errorf("system is in GUID restricted mode but no SdcGUID provided")
 		}
 	case "ApprovedIp":
 		// Allow only approved IP addresses for this SDC
@@ -388,7 +390,11 @@ func (s *System) ApproveSdc(approveSdcParam *types.ApproveSdcParam) (*types.Appr
 			sdcParam.SdcIP = approveSdcParam.SdcIP
 		} else if len(approveSdcParam.SdcIps) > 0 {
 			sdcParam.SdcIps = approveSdcParam.SdcIps
+		} else {
+			return nil, fmt.Errorf("system is in IP restricted mode but no SdcIP or SdcIps provided")
 		}
+	default:
+		return nil, fmt.Errorf("unknown RestrictedSdcMode: %s", s.System.RestrictedSdcMode)
 	}
 
 	err := s.client.getJSONWithRetry(http.MethodPost, path, sdcParam, &resp)
